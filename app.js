f50c59f790ba3036b29fa8061418f347d5d61464
ANUSHRI2002 <anushri046@gmail.com>
2022-12-31 10:11:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type', {
+    strings: [
+    'Hi!!!','I am Anushri','Aspiring Front-End Developer','and just LOVE to code'],
+    typeSpeed:80,
+    backSpeed:50,
+    loop:true,
+    });
